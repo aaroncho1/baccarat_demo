91f04ee3107e82e98f195a801ca7d22dd1f128f4
Aaron <aaroncho10@gmail.com>
2022-03-05 20:18:47 -0500

    changed player to cards class and added run, place wager and sleep and clear methods in game class

--- game.rb
+++ game.rb
@@ -1,9 +1,25 @@
-require_relative "player"
+require_relative "cards"
 require_relative "wager"
 
 class Baccarat
     def initialize(banker, player, wager)
         @banker, @player, @gambler
     end
+
+    def sleep_and_clear
+        sleep 1.75
+        system ("clear")
+        sleep 0.25
+    end
+
+    def place_wager
+
+
+    def run
+        puts "Welcomt to Baccarat! Press 'b' for banker and 'p' for player"
+        sleep_and_clear
+        place_wager
+    end
+
 
 end
