12542c60d599a5a8d1aa5a3407e0d84fb7e19dae
Aaron <aaroncho10@gmail.com>
2022-03-05 22:10:42 -0500

    added drawn cards and score instance methods to banker and player

--- player.rb
+++ player.rb
@@ -1,12 +1,14 @@
 class Player 
 
-    attr_accessor :cards
+    attr_accessor :cards, :drawn_cards, :score
 
-    def initialize
+    def initialize(drawn_cards, score)
         @cards = ([:ace] * 4) + ([:two] * 4) + ([:three] * 4) + 
         ([:four] * 4) + ([:five] * 4) + ([:six] * 4) + ([:seven] * 4) + 
         ([:eight] * 4) + ([:nine] * 4) + ([:ten] * 4) + ([:jack] * 4) + 
         ([:queen] * 4) + ([:king] * 4)
+        @drawn_cards = []
+        @score = 0
     end
 
     def draw_card
